71e78d28131240699e383ce8451bd93ddb16b704
unknown <mohdalfahad5@gmail.com>
2024-10-25 02:24:08 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import { fileURLToPath } from "url";
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+// import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path"; // Importing path for alias resolution
+
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Path alias
     },
   },
-})
+});
+
